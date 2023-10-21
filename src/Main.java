b325315c8e3778313b561a2de3291be1caf8e352
Arnar Ægisson <arnarae@gmail.com>
2023-10-21 17:05:03 +0000

    nýtt main

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) throws Exception{
+        String[] test_path = {"src\\SomeInputs\\SomeInputs\\circle100.txt"};
+        KdTreeVisualizer.main(test_path);
+        //RangeSearchVisualizer.main(test_path);
+        //NearestNeighborVisualizer.main(test_path);
+
+    }
+}
