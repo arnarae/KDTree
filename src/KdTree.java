076ee1bfd58a7b1db5f2166305890676e45d02bd
eggertholm <eggertp19@ru.is>
2023-10-21 14:51:41 +0000

    smá breytingar

--- src/KdTree.java
+++ src/KdTree.java
@@ -1,7 +1,3 @@
-
-/*************************************************************************
- *************************************************************************/
-
 import java.util.Arrays;
 import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.StdDraw;
@@ -40,38 +36,38 @@ public class KdTree {
     }
 
     public void insert(Point2D p) {
-        root = insert(root, p, true, new RectHV(0, 0, 1, 1));
+        root = insert(root, p, true, 0, 0, 1, 1);
     }
 
-    private Node insert(Node node, Point2D p, boolean isVertical, RectHV rect) {
+    private Node insert(Node node, Point2D p, boolean isVertical, double xmin, double ymin, double xmax, double ymax) {
         if (node == null) {
             size++;
-            return new Node(p, rect);
+            return new Node(p, new RectHV(xmin, ymin, xmax, ymax));
         }
 
         if (node.point.equals(p)) {
             return node;
         }
 
-        int cmp;
-        if (isVertical) {
-            cmp = Double.compare(p.x(), node.point.x());
-            if (cmp < 0) {
-                node.left = insert(node.left, p, !isVertical, new RectHV(rect.xmin(), rect.ymin(), node.point.x(), rect.ymax()));
+        int cmp = isVertical ? Double.compare(p.x(), node.point.x()) : Double.compare(p.y(), node.point.y());
+
+        if (cmp < 0) {
+            if (isVertical) {
+                node.left = insert(node.left, p, !isVertical, xmin, ymin, node.point.x(), ymax);
             } else {
-                node.right = insert(node.right, p, !isVertical, new RectHV(node.point.x(), rect.ymin(), rect.xmax(), rect.ymax()));
+                node.left = insert(node.left, p, !isVertical, xmin, ymin, xmax, node.point.y());
             }
         } else {
-            cmp = Double.compare(p.y(), node.point.y());
-            if (cmp < 0) {
-                node.left = insert(node.left, p, !isVertical, new RectHV(rect.xmin(), rect.ymin(), rect.xmax(), node.point.y()));
+            if (isVertical) {
+                node.right = insert(node.right, p, !isVertical, node.point.x(), ymin, xmax, ymax);
             } else {
-                node.right = insert(node.right, p, !isVertical, new RectHV(rect.xmin(), node.point.y(), rect.xmax(), rect.ymax()));
+                node.right = insert(node.right, p, !isVertical, xmin, node.point.y(), xmax, ymax);
             }
         }
 
         return node;
     }
+
 
     public boolean contains(Point2D p) {
         return contains(root, p, true);
@@ -86,12 +82,7 @@ public class KdTree {
             return true;
         }
 
-        int cmp;
-        if (isVertical) {
-            cmp = Double.compare(p.x(), node.point.x());
-        } else {
-            cmp = Double.compare(p.y(), node.point.y());
-        }
+        int cmp = isVertical ? Double.compare(p.x(), node.point.x()) : Double.compare(p.y(), node.point.y());
 
         if (cmp < 0) {
             return contains(node.left, p, !isVertical);
@@ -101,49 +92,49 @@ public class KdTree {
     }
 
     public void draw() {
-    draw(root, true);
-}
-
-private void draw(Node node, boolean isVertical) {
-    if (node != null) {
-        // Draw the point
-        StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.setPenRadius(0.01);
-        node.point.draw();
-
-        // Draw the dividing line based on orientation
-        if (isVertical) {
-            StdDraw.setPenColor(StdDraw.RED);
-            StdDraw.setPenRadius();
-            StdDraw.line(node.point.x(), node.rect.ymin(), node.point.x(), node.rect.ymax());
-        } else {
-            StdDraw.setPenColor(StdDraw.BLUE);
-            StdDraw.setPenRadius();
-            StdDraw.line(node.rect.xmin(), node.point.y(), node.rect.xmax(), node.point.y());
-        }
-
-        // Recursively draw left and right subtrees
-        draw(node.left, !isVertical);
-        draw(node.right, !isVertical);
+        draw(root, true);
     }
-}
+
+    private void draw(Node node, boolean isVertical) {
+        if (node != null) {
+            // Draw the point
+            StdDraw.setPenColor(StdDraw.BLACK);
+            StdDraw.setPenRadius(0.01);
+            node.point.draw();
+
+            // Draw the dividing line based on orientation
+            if (isVertical) {
+                StdDraw.setPenColor(StdDraw.RED);
+                StdDraw.setPenRadius();
+                StdDraw.line(node.point.x(), node.rect.ymin(), node.point.x(), node.rect.ymax());
+            } else {
+                StdDraw.setPenColor(StdDraw.BLUE);
+                StdDraw.setPenRadius();
+                StdDraw.line(node.rect.xmin(), node.point.y(), node.rect.xmax(), node.point.y());
+            }
+
+            // Recursively draw left and right subtrees
+            draw(node.left, !isVertical);
+            draw(node.right, !isVertical);
+        }
+    }
 
     public Iterable<Point2D> range(RectHV rect) {
         Queue<Point2D> pointsInRange = new Queue<>();
         range(root, rect, pointsInRange);
         return pointsInRange;
     }
-    
+
     private void range(Node node, RectHV rect, Queue<Point2D> pointsInRange) {
         if (node != null) {
             if (rect.contains(node.point)) {
                 pointsInRange.enqueue(node.point);
             }
-            
+
             if (node.left != null && rect.intersects(node.left.rect)) {
                 range(node.left, rect, pointsInRange);
             }
-            
+
             if (node.right != null && rect.intersects(node.right.rect)) {
                 range(node.right, rect, pointsInRange);
             }
@@ -154,106 +145,48 @@ private void draw(Node node, boolean isVertical) {
         if (isEmpty()) {
             return null;
         }
-        
+
         return nearest(root, p, root.point, true);
     }
-    
+
     private Point2D nearest(Node node, Point2D query, Point2D closest, boolean isVertical) {
         if (node == null) {
             return closest;
         }
-    
+
         double closestDist = closest.distanceTo(query);
         double currDist = node.point.distanceTo(query);
-    
+
         if (currDist < closestDist) {
             closest = node.point;
         }
-    
+
+        double firstDist, secondDist;
+        Node firstNode, secondNode;
+
         if (isVertical) {
-            if (query.x() < node.point.x()) {
-                closest = nearest(node.left, query, closest, !isVertical);
-                if (node.right != null && node.right.rect.distanceTo(query) < closest.distanceTo(query)) {
-                    closest = nearest(node.right, query, closest, !isVertical);
-                }
-            } else {
-                closest = nearest(node.right, query, closest, !isVertical);
-                if (node.left != null && node.left.rect.distanceTo(query) < closest.distanceTo(query)) {
-                    closest = nearest(node.left, query, closest, !isVertical);
-                }
-            }
+            firstNode = node.left;
+            secondNode = node.right;
+            firstDist = query.x() - node.point.x();
+            secondDist = firstNode.rect.distanceTo(query);
         } else {
-            if (query.y() < node.point.y()) {
-                closest = nearest(node.left, query, closest, !isVertical);
-                if (node.right != null && node.right.rect.distanceTo(query) < closest.distanceTo(query)) {
-                    closest = nearest(node.right, query, closest, !isVertical);
-                }
-            } else {
-                closest = nearest(node.right, query, closest, !isVertical);
-                if (node.left != null && node.left.rect.distanceTo(query) < closest.distanceTo(query)) {
-                    closest = nearest(node.left, query, closest, !isVertical);
-                }
-            }
+            firstNode = query.y() < node.point.y() ? node.left : node.right;
+            secondNode = firstNode == node.left ? node.right : node.left;
+            firstDist = query.y() - node.point.y();
+            secondDist = firstNode.rect.distanceTo(query);
         }
-    
+
+        if (firstDist < 0) {
+            firstNode = secondNode;
+            secondNode = node.right;
+        }
+
+        closest = nearest(firstNode, query, closest, !isVertical);
+
+        if (secondDist < closest.distanceTo(query)) {
+            closest = nearest(secondNode, query, closest, !isVertical);
+        }
+
         return closest;
-    }
-    
-
-    /*******************************************************************************
-     * Test client
-     ******************************************************************************/
-    public static void main(String[] args) {
-        In in = new In();
-        Out out = new Out();
-        int nrOfRecangles = in.readInt();
-        int nrOfPointsCont = in.readInt();
-        int nrOfPointsNear = in.readInt();
-        RectHV[] rectangles = new RectHV[nrOfRecangles];
-        Point2D[] pointsCont = new Point2D[nrOfPointsCont];
-        Point2D[] pointsNear = new Point2D[nrOfPointsNear];
-        for (int i = 0; i < nrOfRecangles; i++) {
-            rectangles[i] = new RectHV(in.readDouble(), in.readDouble(),
-                    in.readDouble(), in.readDouble());
-        }
-        for (int i = 0; i < nrOfPointsCont; i++) {
-            pointsCont[i] = new Point2D(in.readDouble(), in.readDouble());
-        }
-        for (int i = 0; i < nrOfPointsNear; i++) {
-            pointsNear[i] = new Point2D(in.readDouble(), in.readDouble());
-        }
-        KdTree set = new KdTree();
-        for (int i = 0; !in.isEmpty(); i++) {
-            double x = in.readDouble(), y = in.readDouble();
-            set.insert(new Point2D(x, y));
-        }
-        for (int i = 0; i < nrOfRecangles; i++) {
-            // Query on rectangle i, sort the result, and print
-            Iterable<Point2D> ptset = set.range(rectangles[i]);
-            int ptcount = 0;
-            for (Point2D p : ptset)
-                ptcount++;
-            Point2D[] ptarr = new Point2D[ptcount];
-            int j = 0;
-            for (Point2D p : ptset) {
-                ptarr[j] = p;
-                j++;
-            }
-            Arrays.sort(ptarr);
-            out.println("Inside rectangle " + (i + 1) + ":");
-            for (j = 0; j < ptcount; j++)
-                out.println(ptarr[j]);
-        }
-        out.println("Contain test:");
-        for (int i = 0; i < nrOfPointsCont; i++) {
-            out.println((i + 1) + ": " + set.contains(pointsCont[i]));
-        }
-
-        out.println("Nearest test:");
-        for (int i = 0; i < nrOfPointsNear; i++) {
-            out.println((i + 1) + ": " + set.nearest(pointsNear[i]));
-        }
-
-        out.println();
     }
 }
